b64c7935070b71d4e9ad4efff777734d186aacbd
akir94 <akir94@gmail.com>
2017-05-16 14:12:56 +0300

    Changed to write to multiple files using avro's "object container files format"

--- src/main/java/org/z/logdumper/Main.java
+++ src/main/java/org/z/logdumper/Main.java
@@ -1,15 +1,9 @@
 package org.z.logdumper;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 
@@ -21,13 +15,20 @@ import org.apache.avro.util.Utf8;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.streams.StreamsConfig;
 
+import akka.actor.ActorSystem;
+import akka.kafka.ConsumerSettings;
+import akka.kafka.Subscriptions;
+import akka.kafka.javadsl.Consumer;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
@@ -41,134 +42,110 @@ import io.confluent.kafka.serializers.KafkaAvroSerializer;
  *
  */
 public class Main {
-	private static final Duration POLL_INTERVAL = Duration.ofSeconds(1);
-	private static final Duration DUMP_CUTOFF_POINT = Duration.ofMillis(250);
+	private static final String DUMP_DIRECTORY = System.getenv("DUMP_DIRECTORY");
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
-//		Executors.newSingleThreadExecutor().submit(() -> writeSomeData(schemaRegistry));
+		ActorSystem system = ActorSystem.create();
+		ActorMaterializer materializer = ActorMaterializer.create(system);
+
+		Executors.newSingleThreadExecutor().submit(() -> writeSomeData(schemaRegistry));
+		Thread.sleep(2200);
 		
-        try (KafkaConsumer<Object, Object> consumer = createKafkaConsumer(schemaRegistry);
-        		RecordWriter writer = createRecordWriter();) {
-	        List<ConsumerRecord<Object, Object>> recordsToDump = new ArrayList<>();
-	        while(true) {
-	        	Instant start = Instant.now();
-	        	processRecords(consumer.poll(0), recordsToDump, writer);
-	        	Instant end = Instant.now();
-	        	Duration sleepDuration = Duration.between(end, start.plus(POLL_INTERVAL));
-	        	if (!sleepDuration.isNegative()) {
-		        	try {
-						Thread.sleep(sleepDuration.toMillis());
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	        	}
-	        }
-        } catch (IOException e) {
-			throw new ExceptionInInitializerError(e);
-        }
+    	Map<String, List<PartitionInfo>> topics = getAllTopicsAndPartitions();
+    	for(Map.Entry<String, List<PartitionInfo>> entry : topics.entrySet()) {
+    		for (PartitionInfo info : entry.getValue()) {
+    			RecordWriter writer = RecordWriter.create(DUMP_DIRECTORY, info.topic(), info.partition());
+    			createSource(system, schemaRegistry, info.topic(), info.partition())
+    				.to(Sink.foreach(r -> writer.accept(r)))
+    				.run(materializer);
+    		}
+    	}
+    	
+    	Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				materializer.shutdown();
+				system.terminate();
+			}
+		});
+    	
+    	while (true) {
+    		Thread.sleep(10000);
+    	}
 	}
 	
-	private static KafkaConsumer<Object, Object> createKafkaConsumer(SchemaRegistryClient schemaRegistry) {
+	private static Map<String, List<PartitionInfo>> getAllTopicsAndPartitions() {
+		KafkaConsumer<Object, Object> consumer = createKafkaConsumer();
+		subscribeConsumerToAllTopics(consumer);
+		return consumer.listTopics();
+	}
+
+	private static KafkaConsumer<Object, Object> createKafkaConsumer() {
 		Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "log-dumper");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put("group.id", "groupId");
-        KafkaConsumer<Object, Object> consumer = new KafkaConsumer<>(props, 
-        		new KafkaAvroDeserializer(schemaRegistry), new KafkaAvroDeserializer(schemaRegistry));
-        consumer.subscribe(Pattern.compile(".*"), new ConsumerRebalanceListener() { //no-op consumer rebalance listener
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+        return new KafkaConsumer<>(props, 
+        		new KafkaAvroDeserializer(), new KafkaAvroDeserializer());
+	}
+	
+	private static void subscribeConsumerToAllTopics(KafkaConsumer<Object, Object> consumer) {
+		consumer.subscribe(Pattern.compile(".*"), new ConsumerRebalanceListener() { //no-op consumer rebalance listener
 			@Override
 			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {}
 			
 			@Override
 			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {}
 		});
-        return consumer;
 	}
 	
-	private static RecordWriter createRecordWriter() {
-		try {
-			return new RecordWriter(new FileOutputStream("dump_file"));
-		} catch (FileNotFoundException e) {
-			throw new ExceptionInInitializerError(e);
-		}
+	private static Source<ConsumerRecord<Object, Object>, Consumer.Control> createSource(ActorSystem system, 
+			SchemaRegistryClient schemaRegistry, String topic, int partition) {
+		ConsumerSettings<Object, Object> settings = 
+				ConsumerSettings.create(system, new KafkaAvroDeserializer(schemaRegistry), new KafkaAvroDeserializer(schemaRegistry))
+			        .withBootstrapServers(System.getenv("KAFKA_ADDRESS"))
+			        .withGroupId("group2")
+			        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		return Consumer.plainSource(settings, 
+				Subscriptions.assignmentWithOffset(new TopicPartition(topic, partition), 0));
 	}
 	
-	/**
-	 * It is important to note that it is possible to poll twice, and in the second poll
-	 * receive a record that arrived earlier than a record we received in the previous poll.
-	 * 
-	 * To combat that, in every poll we split the records into two groups- those that arrived in the
-	 * first part of the interval, and those that arrived in the second part.
-	 * Those that arrived in the first part, we write immediately. Those that arrived in the second
-	 * part, we put aside for the next poll.
-	 * 
-	 * @param records The records received in the current poll
-	 * @param recordsToDump The records we put aside in the previous poll
-	 * @param writer
-	 */
-	private static void processRecords(ConsumerRecords<Object, Object> records, 
-			List<ConsumerRecord<Object, Object>> recordsToDump, RecordWriter writer) {
-		Instant dumpCutoffPoint = Instant.now().minus(DUMP_CUTOFF_POINT);
-		List<ConsumerRecord<Object, Object>> recordsToDumpLater = new ArrayList<>();
-		for (ConsumerRecord<Object, Object> record : records) {
-    		if (record.timestamp() < dumpCutoffPoint.getEpochSecond()) {
-    			recordsToDump.add(record);
-    		} else {
-    			recordsToDumpLater.add(record);
-    		}
-    	}
-		dumpRecords(recordsToDump, recordsToDumpLater, writer);
+	private static void writeSomeData(SchemaRegistryClient schemaRegistry) {
+		Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "log-dumper");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put("group.id", "groupId");
+        KafkaProducer<Object, Object> producer = new KafkaProducer<>(props, 
+        		new KafkaAvroSerializer(schemaRegistry), new KafkaAvroSerializer(schemaRegistry));
+        
+        Schema schema = SchemaBuilder.builder().record("record").fields()
+        		.name("field").type().longType().noDefault()
+        		.endRecord();
+        
+        Utf8 key1 = new Utf8("key1");
+        GenericRecord value1 = new GenericRecordBuilder(schema).set("field", 1234L).build();
+        producer.send(new ProducerRecord<Object, Object>("test", key1, value1));
+        try{
+        	Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        	
+        }
+        
+        Utf8 key2 = new Utf8("key2");
+        GenericRecord value2 = new GenericRecordBuilder(schema).set("field", 1234L).build();
+        producer.send(new ProducerRecord<Object, Object>("test", key2, value2));
+        try{
+        	Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        	
+        }
+        
+        Utf8 key3 = new Utf8("key3");
+        GenericRecord value3 = new GenericRecordBuilder(schema).set("field", 1234L).build();
+        producer.send(new ProducerRecord<Object, Object>("test", key3, value3));
+        
+        producer.close();
 	}
-
-	private static void dumpRecords(List<ConsumerRecord<Object, Object>> recordsToDump, 
-			List<ConsumerRecord<Object, Object>> recordsToDumpLater, RecordWriter writer) {
-		recordsToDump.sort((record1, record2) -> Long.compare(record1.timestamp(), record2.timestamp()));
-		writer.write(recordsToDump);
-		recordsToDump.clear();
-		recordsToDump.addAll(recordsToDumpLater);
-	}
-	
-//	private static void writeSomeData(SchemaRegistryClient schemaRegistry) {
-//		try{
-//        	Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//        	
-//        }
-//		
-//		Properties props = new Properties();
-//        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "log-dumper");
-//        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//        props.put("group.id", "groupId");
-//        KafkaProducer<Object, Object> producer = new KafkaProducer<>(props, 
-//        		new KafkaAvroSerializer(schemaRegistry), new KafkaAvroSerializer(schemaRegistry));
-//        
-//        Schema schema = new Schema.Parser().parse("{\"type\": \"record\", \"name\": \"name\", \"fields\": [{\"name\": \"field\", \"type\": \"long\"}]}");
-//        Utf8 key1 = new Utf8("key1");
-//        GenericRecord value1 = new GenericRecordBuilder(schema).set("field", 1234L).build();
-//        producer.send(new ProducerRecord<Object, Object>("test", key1, value1));
-//        
-//        try{
-//        	Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//        	
-//        }
-//        
-//        Utf8 key2 = new Utf8("key2");
-//        GenericRecord value2 = new GenericRecordBuilder(schema).set("field", 1234L).build();
-//        producer.send(new ProducerRecord<Object, Object>("test", key2, value2));
-//        
-//        try{
-//        	Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//        	
-//        }
-//        
-//        Utf8 key3 = new Utf8("key3");
-//        GenericRecord value3 = new GenericRecordBuilder(schema).set("field", 1234L).build();
-//        producer.send(new ProducerRecord<Object, Object>("test", key3, value3));
-//	}
 }
--- src/main/java/org/z/logdumper/RecordWriter.java
+++ src/main/java/org/z/logdumper/RecordWriter.java
@@ -1,78 +1,74 @@
 package org.z.logdumper;
 
-import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
-import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.List;
+import java.util.function.Consumer;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.util.Utf8;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class RecordWriter implements Closeable{
-	private OutputStream outputStream;
-	private ByteArrayOutputStream buffer;
-	private DataOutputStream bufferWriter;
-	private BinaryEncoder encoder;
-	private DatumWriter<GenericRecord> datumWriter;
+public class RecordWriter implements Consumer<ConsumerRecord<Object, Object>>{
+	File destinationFile;
+	DataFileWriter<GenericRecord> fileWriter;
+	GenericRecordBuilder dumpRecordBuilder;
 	
-	public RecordWriter(OutputStream outputStream) {
-		this.outputStream = outputStream;
-		this.buffer = new ByteArrayOutputStream();
-		this.bufferWriter = new DataOutputStream(buffer);
-		this.encoder = EncoderFactory.get().binaryEncoder(bufferWriter, null);
-		this.datumWriter = new GenericDatumWriter<>();
+	private RecordWriter(File destinationFile) {
+		this.destinationFile = destinationFile;
+		// both initialized when first record arrives, because we need the schema of the values
+		this.fileWriter = null; 
+		this.dumpRecordBuilder = null;
 	}
 	
-	public void write(List<ConsumerRecord<Object, Object>> records) {
-		for (ConsumerRecord<Object, Object> record : records) {
-			System.out.println("writing record");
-			try {
-				writeData(record);
-			} catch (IOException e) {
-				System.out.println("Failed to write ConsumerRecord to temporary buffer");
-				e.printStackTrace();
-			}
-			System.out.println("done");
-		}
+	public static RecordWriter create(String dumpDirectory, String topic, int partition) {
+		File destinationFile = new File(dumpDirectory, topic + "-" + partition + ".dump");
+		return new RecordWriter(destinationFile);
+	}
+	
+	@Override
+	public void accept(ConsumerRecord<Object, Object> record) {
+		System.out.println("accepting record from " + record.topic() + "-" + record.partition());
+		GenericRecord value = (GenericRecord) record.value();
+		String key = (String) record.key();
 		try {
-			buffer.writeTo(outputStream);
+			initFileWriterIfNeeded(value, record.topic(), record.partition());
+			fileWriter.append(createDumpRecord(value, key, record.timestamp()));
 		} catch (IOException e) {
-			System.out.println("Failed to write temporary buffer to outputStream");
+			System.out.println("Failed to accept record on topic " + record.topic() + " and partition " + record.partition());
 			e.printStackTrace();
 		}
-		buffer.reset();
 	}
 	
-	private void writeData(ConsumerRecord<Object, Object> record) throws IOException {
-		writeKeyAndValue((String) record.key(), (GenericRecord) record.value());
-		bufferWriter.writeUTF(record.topic());
-		bufferWriter.writeInt(record.partition());
-		bufferWriter.writeLong(record.timestamp());
-		bufferWriter.flush();
-	}
-	
-	private void writeKeyAndValue(String key, GenericRecord value) throws IOException {
-		datumWriter.setSchema(value.getSchema());
-		datumWriter.write(value, encoder);
-		if (key != null) {
-			encoder.writeBoolean(true);
-			encoder.writeString(key);
-		} else {
-			encoder.writeBoolean(false);
+	private void initFileWriterIfNeeded(GenericRecord value, String topic, int partition) throws IOException {
+		if(fileWriter == null) {
+			Schema schema = initSchema(value.getSchema());
+			DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
+			DataFileWriter<GenericRecord> fileWriter = new DataFileWriter<>(datumWriter);
+			File destination = new File(System.getenv("DUMP_DIR"), topic + "-" + partition + ".dump");
+			fileWriter.create(schema, destination);
+			this.fileWriter = fileWriter;
+			this.dumpRecordBuilder = new GenericRecordBuilder(schema);
 		}
-		encoder.flush();
 	}
-
-	@Override
-	public void close() throws IOException {
-		outputStream.close();
-		bufferWriter.close();
+	
+	private Schema initSchema(Schema valueSchema) {
+		return SchemaBuilder.builder().record("DumpRecord").fields()
+			.name("key").type().stringType().noDefault()
+			.name("value").type(valueSchema).noDefault()
+			.name("timestamp").type().longType().noDefault()
+			.endRecord();
+	}
+	
+	private GenericRecord createDumpRecord(GenericRecord value, String key, long timestamp) {
+		return this.dumpRecordBuilder
+			.set("key", key)
+			.set("value", value)
+			.set("timestamp", timestamp)
+			.build();
 	}
 }
